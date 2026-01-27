4eb7554ff3c9fb9fe89faa29d2a9c46d7edd0080
abdalrahmanabnmohammed-png <abdalrahman.abn.mohammed@gmail.com>
2026-01-28 02:01:12 +0300

    Update route.js

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,13 +1,23 @@
 import NextAuth from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 
-const handler = NextAuth({
+export const authOptions = {
   providers: [
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID,
       clientSecret: process.env.DISCORD_CLIENT_SECRET,
     }),
   ],
-});
+  callbacks: {
+    // هذه الخطوة ضرورية لنقل الـ ID من ديسكورد إلى الموقع
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.sub; 
+      }
+      return session;
+    },
+  },
+};
 
+const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
