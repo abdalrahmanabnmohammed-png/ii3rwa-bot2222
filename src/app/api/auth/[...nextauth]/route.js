f827ef0f31c47875749d34c33aa093de9f2a2f35
abdalrahmanabnmohammed-png <abdalrahman.abn.mohammed@gmail.com>
2026-01-28 01:35:11 +0300

    Update route.js

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -6,18 +6,8 @@ const handler = NextAuth({
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID,
       clientSecret: process.env.DISCORD_CLIENT_SECRET,
-      authorization: { params: { scope: 'identify guilds' } },
     }),
   ],
-  callbacks: {
-    async session({ session, token }) {
-      session.user.id = token.sub;
-      return session;
-    },
-  },
-  pages: {
-    signIn: '/login',
-  },
 });
 
 export { handler as GET, handler as POST };
