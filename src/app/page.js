9f2b16ba0a1176047b31a790260b9b5062465981
abdalrahmanabnmohammed-png <abdalrahman.abn.mohammed@gmail.com>
2026-01-28 00:11:26 +0300

    Create page.js

---
+++ src/app/page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <h1>لوحة تحكم بوتات ii3RwA قيد الإنشاء 🚀</h1>
+    </div>
+  )
+}
